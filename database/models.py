a12d9c3ad9e04608978867156f803b531101c3d2
Yuji Kunitake <yujikunitake@ghr.com.br>
2024-12-30 12:08:38 -0300

    database.models.py setup complete

--- database/models.py
+++ database/models.py
@@ -1,5 +1,25 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.sql import func
 
 
+class Publishers(Base):
+    __tablename__ = "publishers"
 
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+
+class Articles(Base):
+    __tablename__ = "articles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    publication_date = Column(DateTime, default=func.now())
+    publisher_id = Column(Integer, ForeignKey("publishers.id"))
